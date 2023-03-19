cb2523bbec33747401916b7e47ecb0a83247395a
Sadık Sünbül <109942759+artredline@users.noreply.github.com>
2023-03-19 12:56:38 +0300

    Özel sayıların buyuk bır cogunlugu yazıldı

---
+++ özelsayılar/Palindromik_sayılar/AmstrongSayı/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+//Belirli bir tabanda basamaklarının, sayının basamak sayısı kadar kuvvetlerinin toplamına eşit olan sayılara Armstrong sayısı denir.
+
+
+Console.WriteLine("Bır sayı gırınız:");
+int sayi=int.Parse(Console.ReadLine());
+int temp = sayi;
+int toplam = 0;
+while (temp>=1) 
+{
+    int mod = temp % 10;
+    temp=temp/10;
+    toplam=toplam+(int)Math.Pow(mod, 3);
+}
+if (sayi==toplam)
+{
+    Console.WriteLine("Girdiğiniz sayı:" + sayi);
+    Console.WriteLine("Sayıların basamakaları toplamı :" + toplam);
+    Console.WriteLine("Gırdıgınız sayı Amstrong bır sayıdır");
+
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı:" + sayi);
+    Console.WriteLine("Sayıların basamakaları toplamı :" + toplam);
+    Console.WriteLine("Gırdıgınız sayı Amstrong bır sayı degıldır");
+
+}
---
+++ özelsayılar/Palindromik_sayılar/Zengin_Sayılar/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Kendisi hariç pozitif tam sayı bölenlerinin toplamı kendisinden büyük olan sayılara zengin (abundant) sayı denir.
+
+Console.WriteLine("Bır sayı gırınız:");
+int sayi=int.Parse(Console.ReadLine());
+int toplam = 0;
+for (int i = (sayi-1); i > 0; i--)
+{
+	if (sayi%i==0)
+	{
+		toplam = toplam + i;
+	}
+}
+if (toplam>sayi)
+{
+	Console.WriteLine("Sayi:" + sayi);
+	Console.WriteLine("Sayının bolenlerı toplamı:" + toplam);
+    Console.WriteLine("Girdiğiniz sayı Zemgın sayıdır");
+}
+else
+{
+    Console.WriteLine("Sayi:" + sayi);
+    Console.WriteLine("Sayının bolenlerı toplamı:" + toplam);
+    Console.WriteLine("Girdiğiniz sayı Zemgın sayı değildir");
+}
