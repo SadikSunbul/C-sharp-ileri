7759727526d0db0458c9954e8afe7da0e28368a5
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-04-10 01:41:54 +0300

    task await async

---
+++ CRUD/ÖrnekProje/Migrations/20230406122057_mig_1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ÖrnekProje.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "A1Sequence");
+
+            migrationBuilder.CreateSequence(
+                name: "deger",
+                startValue: 900L);
+
+            migrationBuilder.CreateTable(
+                name: "A1",
+                columns: table => new
+                {
+                    AId = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR [A1Sequence]"),
+                    Aacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_A1", x => x.AId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Asınıfıs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Aacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bacıklama = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cacıklama = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asınıfıs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "B1",
+                columns: table => new
+                {
+                    AId = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR [A1Sequence]"),
+                    Aacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_B1", x => x.AId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "C1",
+                columns: table => new
+                {
+                    AId = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR [A1Sequence]"),
+                    Aacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_C1", x => x.AId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hesaplar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HesapAdı = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hesaplar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kişiler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyadi = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kişiler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kurslar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KursAdi = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kurslar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sınıflar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sınıfno = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sınıflar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "xsınıfı",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Xacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_xsınıfı", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kullanıcılar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyadı = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HesapId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kullanıcılar", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Kullanıcılar_Hesaplar_HesapId",
+                        column: x => x.HesapId,
+                        principalTable: "Hesaplar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kişikurslar",
+                columns: table => new
+                {
+                    KişiId = table.Column<int>(type: "int", nullable: false),
+                    KursId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kişikurslar", x => new { x.KursId, x.KişiId });
+                    table.ForeignKey(
+                        name: "FK_Kişikurslar_Kişiler_KişiId",
+                        column: x => x.KişiId,
+                        principalTable: "Kişiler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Kişikurslar_Kurslar_KursId",
+                        column: x => x.KursId,
+                        principalTable: "Kurslar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Öğrenciler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    sequences = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR deger"),
+                    Soyadı = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Tc = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SınıfNo = table.Column<int>(type: "int", nullable: false),
+                    KayıtZamanı = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2023, 4, 6, 15, 20, 57, 863, DateTimeKind.Local).AddTicks(9324))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Öğrenciler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Öğrenciler_Sınıflar_SınıfNo",
+                        column: x => x.SınıfNo,
+                        principalTable: "Sınıflar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ysınıfı",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Yacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ysınıfı", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ysınıfı_xsınıfı_Id",
+                        column: x => x.Id,
+                        principalTable: "xsınıfı",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Zsınıfı",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Zacıklama = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zsınıfı", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Zsınıfı_Ysınıfı_Id",
+                        column: x => x.Id,
+                        principalTable: "Ysınıfı",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kişikurslar_KişiId",
+                table: "Kişikurslar",
+                column: "KişiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kullanıcılar_HesapId",
+                table: "Kullanıcılar",
+                column: "HesapId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Öğrenciler_Adi_Soyadı",
+                table: "Öğrenciler",
+                columns: new[] { "Adi", "Soyadı" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Öğrenciler_SınıfNo",
+                table: "Öğrenciler",
+                column: "SınıfNo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "A1");
+
+            migrationBuilder.DropTable(
+                name: "Asınıfıs");
+
+            migrationBuilder.DropTable(
+                name: "B1");
+
+            migrationBuilder.DropTable(
+                name: "C1");
+
+            migrationBuilder.DropTable(
+                name: "Kişikurslar");
+
+            migrationBuilder.DropTable(
+                name: "Kullanıcılar");
+
+            migrationBuilder.DropTable(
+                name: "Öğrenciler");
+
+            migrationBuilder.DropTable(
+                name: "Zsınıfı");
+
+            migrationBuilder.DropTable(
+                name: "Kişiler");
+
+            migrationBuilder.DropTable(
+                name: "Kurslar");
+
+            migrationBuilder.DropTable(
+                name: "Hesaplar");
+
+            migrationBuilder.DropTable(
+                name: "Sınıflar");
+
+            migrationBuilder.DropTable(
+                name: "Ysınıfı");
+
+            migrationBuilder.DropTable(
+                name: "xsınıfı");
+
+            migrationBuilder.DropSequence(
+                name: "A1Sequence");
+
+            migrationBuilder.DropSequence(
+                name: "deger");
+        }
+    }
+}
---
+++ CRUD/ÖrnekProje/Program.cs
@@ -0,0 +1,290 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Reflection;
+
+TetstContex contex = new();
+
+Console.WriteLine("Tamam");
+Console.ReadLine();
+
+
+
+#region birebir
+public class kullanıcı
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+    public string Soyadı { get; set; }
+    public int HesapId { get; set; }
+    public Hesap Hesap { get; set; }
+}
+public class Hesap
+{
+    public int Id { get; set; }
+    public string HesapAdı { get; set; }
+    public kullanıcı Kullanıcı { get; set; }
+
+}
+#endregion
+
+#region birecok
+public class Öğrenci
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+    public string Laylaylom { get; set; }
+    public int sequences { get; set; }
+    public string Soyadı { get; set; }
+    public string _tc;
+    public string Tc { get => _tc.Substring(0,4); set=>_tc=value; }
+    public int SınıfNo { get; set; }
+    public Sınıf Sınıf { get; set; }
+}
+
+public class Sınıf
+{
+    public int Id { get; set; }
+    public string Sınıfno { get; set; }
+    public ICollection<Öğrenci> Öğrenciler { get; set; }
+}
+#endregion
+
+#region Çoka çok
+public class kişi
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+    public string Soyadi { get; set; }
+    public ICollection<kişikurs> Kurslar { get; set; }
+}
+public class Kurs
+{
+    public int Id { get; set; }
+    public string KursAdi { get; set; }
+    public ICollection<kişikurs> Kişiler { get; set; }
+}
+
+public class kişikurs
+{
+    public int KişiId { get; set; }
+    public int KursId { get; set; }
+    public kişi Kişi { get; set; }
+    public Kurs Kurs { get; set; }
+}
+#endregion
+
+#region TPT
+
+public class Asınıfı
+{
+    public int Id { get; set; }
+    public string Aacıklama { get; set; }
+
+}
+public class Bsınıfı:Asınıfı
+{
+   
+    public string Bacıklama { get; set; }
+
+}
+public class Csınıfı:Bsınıfı
+{
+   
+    public string Cacıklama { get; set; }
+}
+#endregion
+#region TPH
+public class   Xsınıfı
+{
+    public int Id { get; set; }
+    public string Xacıklama { get; set; }
+
+}
+public class Ysınıfı : Xsınıfı
+{
+    
+    public string Yacıklama { get; set; }
+
+}
+public class Zsınıfı : Ysınıfı
+{
+    
+    public string Zacıklama { get; set; }
+}
+#endregion
+#region TPC
+public class A1
+{
+    public int AId { get; set; }
+    public string Aacıklama { get; set; }
+
+}
+public class B1 : A1
+{
+    
+    public string Bacıklama { get; set; }
+
+}
+public class C1 : B1
+{
+    
+    public string Cacıklama { get; set; }
+}
+#endregion
+
+public class TetstContex :DbContext
+{
+    #region DBSETLER
+    public DbSet<Öğrenci> Öğrenciler { get; set; }
+    public DbSet<Sınıf> Sınıflar { get; set; }
+    public DbSet<kullanıcı> Kullanıcılar { get; set; }
+    public DbSet<Hesap> Hesaplar { get; set; }
+    public DbSet<kişi> Kişiler { get; set; }
+    public DbSet<Kurs> Kurslar { get; set; }
+    public DbSet<kişikurs> Kişikurslar { get; set; }
+    public DbSet<Asınıfı> Asınıfıs { get; set; }
+    public DbSet<Bsınıfı> Bsınıfıs { get; set; }
+    public DbSet<Csınıfı> Csınıfıs { get; set; }
+    public DbSet<Xsınıfı> Xsınıfıs { get; set; }
+    public DbSet<Ysınıfı> Ysınıfıs { get; set; }
+    public DbSet<Zsınıfı> Zsınıfıs { get; set; }
+    public DbSet<A1> A1 { get; set; }
+    public DbSet<B1> B1 { get; set; }
+    public DbSet<C1> C1 { get; set; }
+    #endregion
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=Test;User ID=SA;Password=Viabelli34*.;TrustServerCertificate=True");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.HasSequence("deger")
+            .StartsAt(900);
+    }
+}
+
+#region Configuration
+public class ÖğrenciConfigration : IEntityTypeConfiguration<Öğrenci>
+{
+    public void Configure(EntityTypeBuilder<Öğrenci> builder)
+    {
+        builder.HasOne(i => i.Sınıf)
+            .WithMany(i => i.Öğrenciler)
+            .HasForeignKey(i => i.SınıfNo);
+
+        builder.Property(i => i.Tc)
+            .HasField(nameof(Öğrenci._tc))
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
+
+        builder.Property<DateTime>("KayıtZamanı")
+            .HasDefaultValue(DateTime.Now);
+        builder.HasIndex(i => new { i.Adi, i.Soyadı });
+
+        builder.Ignore(i => i.Laylaylom);
+
+        builder.HasQueryFilter(i => i.Id > 0);
+
+        builder.Navigation(i => i.Sınıf)
+            .AutoInclude();
+
+        builder.Property(i => i.sequences)
+            .HasDefaultValueSql("NEXT VALUE FOR deger");
+    }
+}
+
+public class kullanıcıConfigration : IEntityTypeConfiguration<kullanıcı>
+{
+    public void Configure(EntityTypeBuilder<kullanıcı> builder)
+    {
+        builder.HasOne(i => i.Hesap)
+            .WithOne(i => i.Kullanıcı)
+            .HasForeignKey<kullanıcı>(i => i.HesapId);
+    }
+}
+
+public class KişiKursConfigration : IEntityTypeConfiguration<kişikurs>
+{
+    public void Configure(EntityTypeBuilder<kişikurs> builder)
+    {
+        builder.HasKey(i => new { i.KursId, i.KişiId });
+
+        builder.HasOne(i => i.Kişi)
+            .WithMany(i => i.Kurslar)
+            .HasForeignKey(i => i.KişiId);
+
+        builder.HasOne(i => i.Kurs)
+            .WithMany(i => i.Kişiler)
+            .HasForeignKey(i => i.KursId);
+
+
+    }
+}
+#region TPH
+public class xsınıfıConfigration : IEntityTypeConfiguration<Xsınıfı>
+{
+    public void Configure(EntityTypeBuilder<Xsınıfı> builder)
+    {
+        builder.ToTable("xsınıfı");
+    }
+}
+public class ysınıfıConfigration : IEntityTypeConfiguration<Ysınıfı>
+{
+    public void Configure(EntityTypeBuilder<Ysınıfı> builder)
+    {
+        builder.ToTable("Ysınıfı");
+    }
+}
+public class ZsınıfıConfigration : IEntityTypeConfiguration<Zsınıfı>
+{
+    public void Configure(EntityTypeBuilder<Zsınıfı> builder)
+    {
+        builder.ToTable("Zsınıfı");
+    }
+}
+#endregion
+#region TPC
+public class A1ConfıgratıonÇ : IEntityTypeConfiguration<A1>
+{
+    public void Configure(EntityTypeBuilder<A1> builder)
+    {
+        builder.HasKey(i => i.AId);
+        builder.UseTpcMappingStrategy();
+    }
+}
+#endregion
+#endregion
+
+#region Fonksiyonlar CURUD işlemleri
+
+public class Curud
+{
+
+    public async void Ekle<T>(T veri, TetstContex contex) where T:class
+    {
+        await contex.Set<T>().AddAsync(veri);
+        drektSavechanges(contex);
+    }
+
+    public async void sil<T>(T veri, TetstContex contex) where T : class
+    {
+        contex.Set<T>().Remove(veri);
+        drektSavechanges(contex);
+    }
+
+    public void güncelle<T>(T veri, TetstContex contex) where T : class
+    {
+        contex.Set<T>().Update(veri);
+        drektSavechanges(contex);
+    }
+
+    public async void drektSavechanges(TetstContex contex) 
+    {
+        await contex.SaveChangesAsync();
+    }
+}
+
+#endregion
