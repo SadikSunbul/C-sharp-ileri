51de2e9d939b87f50df3f330e93d08940044baf0
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-04-11 01:28:07 +0300

    comıt

---
+++ csharpİleriDüzey/a/Program.cs
@@ -0,0 +1,19 @@
+﻿
+ab();
+
+
+Console.WriteLine("asdasfassddg"+1);
+
+void ab()
+{
+    using c a = new();
+    
+}
+
+class c : IDisposable
+{
+    public void Dispose()
+    {
+        
+    }
+}
