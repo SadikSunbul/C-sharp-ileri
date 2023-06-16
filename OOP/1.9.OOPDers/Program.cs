f61febd68e5ae5323cc34d970d815cd0b394cae3
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-06-16 14:51:13 +0300

    Inheritance (Kalıtım) 2  base derived eklendi

--- OOP/1.9.OOPDers/Program.cs
+++ OOP/1.9.OOPDers/Program.cs
@@ -10,12 +10,12 @@ namespace _1._9.OOPDers
         {
             //Kalıtım Inheritance
             Kadın kadın = new();
-           
+
         }
     }
 
 
-    #region Inheritance (Kalıtım)
+    #region Inheritance (Kalıtım) 1
     //oop nın en önemli özellğidir
     //Üretilen nesneler farklı nesnelere özelliklerini aktarabilmekte ve böylece hiyerarşik bir düzenleme yapılabilmektedir 
     //Aynı aile grubundan gelen nesnelerin ya da yatayda eşit seviyede olan tüm olguların benzer özelliklerini tekrar tekrar herbirinde tanımlamaktansa bir üst sınıfta tanımlanmasını ve her bir sınıfın bu özellikleri üst sınıftan kalıtmsal olarak almasını sağlamaktadır.
@@ -29,14 +29,91 @@ namespace _1._9.OOPDers
     {
         public int Yaş { get; set; } //kalıtıma gider
         private string İsim { get; set; } //kalıtımda gitmez
-        public  string Soyisim { get; set; } 
+        public string Soyisim { get; set; }
 
     }
 
-    class Kadın:İnsan  //Kadın insandan kalıtım alır 
+    class Kadın : İnsan  //Kadın insandan kalıtım alır 
     {
         public bool Makyaj { get; set; }
 
+    }
+
+    #endregion
+
+    #region Inheritance (Kalıtım) 2  
+    //Base class ve Derived class nedir?
+
+
+    //buarada Base class == Araba olur Derived (child) class == Opel olur 
+    class Araba
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Km { get; set; }
+
+    }
+
+    class Opel : Araba
+    {
+
+    }
+
+    class Astra : Opel  //Opel base class astra Derived class ama Araba base class degıldır bu sınıf ıcın bır sınıfın sadece 1 tane base classı olabılır
+    {
+
+    }
+
+    /*
+     Bir class'ın sade ve sadece bir Base Class'ı olur dedik.
+Bunun nedeni, C# programlama dilinde bir class'ın sade ve sadece tek bir class'tan türetilmesine
+izin verilmektedir! Aynı anda birden fazla class•tan türeme işlemi gerçekleştirilemez!
+    İleride bu şekilde birden fazla kalıtım
+    tanımlamasının
+    yapılabildiğini göreceksiniz.
+    Lakin orada da göreceksiniz ki Z ve W bir
+    sınıf olmayacaktır!
+     */
+
+    //Kalıtımda nesne uretım sırası 
+    /*
+     Bir sınıftan nesne üretimi yapılırken kalıtım aldığı depğer varsa eğer Önce o sınıflardan SIRAYLA nesne üretilir. atadan baslar gelır 
+     */
+
+
+    //BİR SINIFIN BASE CLASS CONSTRUCTERINA ULASIM
+
+    class A
+    {
+        public A()
+        {
+            
+        }
+        public A(int a)
+        {
+             
+        }
+    }
+
+    class B:A
+    {
+        public B():base()  //A nın parametresız halını tetıkler bunu yazmasakta olur cunku default olarak ztn bu calısır 
+        {
+            
+        }
+        public B(int a) : base() //buda aynı sekılde bos olanı tetıkler
+        {
+
+        }
+        public B(int a,string b) : base(a) //burada int parameterlı olanı tetıkler
+        {
+
+        }
+
+        public B(string b):this (12,b) //bi ustekı ctoru tetıkler ilk 
+        {
+
+        }
     }
 
     #endregion
