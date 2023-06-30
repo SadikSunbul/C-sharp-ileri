e009703d26ea2ba8d4d6ec201651b12c1f43c23b
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-06-30 23:05:58 +0300

    interface 1 işlendi

--- OOP/2.6.OOPDerss/Program.cs
+++ OOP/2.6.OOPDerss/Program.cs
@@ -7,7 +7,18 @@ namespace _2._6.OOPDerss
         static void Main(string[] args)
         {
             //İnterfaceler
+            MyClass5 my = new MyClass5();
 
+            Ia ıa = my;
+
+            Ib ıb = my;
+
+            Ic ıc = my;
+
+            denemea denemea = new();
+
+            Iz ız = denemea;
+            ız.y();
         }
     }
 
@@ -122,11 +133,163 @@ namespace _2._6.OOPDerss
 
     #endregion
 
-    #region İnterface kullanımı 
-    
+    //İnterfaceler sınıfların imzalarıdır
 
+    #region İnterface Implementation Yöntemleri 
+    //alt enter yap ımplament et 
+    class MyClass1 : IMyInterface2
+    {
+        public int z { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void x()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void y(int a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //Tum uyelerı acık uygula dedıgımızde farklı gelıcek burası ılerıkı dersın konusu orada detaylıca ıslenıcek
+    class MyClass2 : IMyInterface2
+    {
+        int IMyInterface2.z { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        void IMyInterface2.x()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMyInterface2.y(int a)
+        {
+            throw new NotImplementedException();
+        }
+    }
 
     #endregion
 
+    #region İnterfaceler de coklu kalıtım durumu
+    interface Ia
+    {
+        void A();
+    }
+    interface Ib
+    {
+        void B();
+    }
+    interface Ic
+    {
+        void C();
+    }
+    class MyClass5 : Ia, Ib, Ic //hepsı ıplament edildi
+    {
+        //Myclass5 i larılıya bılenler Ia Ib Ic 
+        /*
+        Myclass5 ımplament=Myclass5 new();
+        Ia ımplament=Myclass5 new();
+        Ib ımplament=Myclass5 new();
+        Ic ımplament=Myclass5 new();
+        bunların hepsı calısıcaktır
+         */
+        public void A()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void B()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void C()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+
+    #region İnterfacenın İnterfaceden türemesi
+    //bır interface bırden fazla interfaceden türiyebilir
+
+    interface Ix
+    {
+        void x();
+    }
+    interface Iy:Ix
+    {
+        void y();
+    }
+    interface Iz:Iy,Iq //buradakı kalıtım farklıdır class ların kalıtımına benzemez 
+    {
+        void z();
+    }
+
+    interface Iq
+    {
+        void q();
+    }
+
+    class denemea : Iz
+    {
+        public void q()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void x()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void y()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void z()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+
+    #region Bir class ınherıtance ıle bırlıkte ıplamentation nasıl yapılır
+    //yanı bır sınıf hem bır sınıfatan turıycek hemde ınterface ımplament edıcek 
+
+    // ılk once classtan kalıtım almalı sonradan interfaceden implament alınmalıdır
+
+    //C# da bır sınıf hem bır calss dan hemde (aynı anda) abstract calss tan turiyemez hata alırız ama istediğimiz kadar interfaceden turıye bılrıler 
+    interface Ide
+    {
+        void x();
+    }
+    interface Iye
+    {
+        void y();
+    }
+    class deneem1
+    {
+        void deneme()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class deneme2 : deneem1, Ide, Iye
+    {
+        public void x()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void y()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    #endregion
     #endregion
 }
+
