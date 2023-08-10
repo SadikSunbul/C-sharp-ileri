d45823275bd0a228d1820ffc0eebf7b4783c3a70
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-08-10 17:43:54 +0300

    RabbitMQ yontemı eklendi

---
+++ Microservices/Microservices-BağlantıTürleri/MessageBroker/Servic1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Publisher
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://xomvilok:bzp8Z98qipQkPmCF5a26xxwwoE1ejUOh@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(exchange: "direct-exchande-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj :");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    chanel.BasicPublish(
+        exchange: "direct-exchande-example",
+         routingKey: "direct-queue-example",
+         body: byteMessage
+        );
+}
+
+Console.ReadLine();
---
+++ Microservices/Microservices-BağlantıTürleri/MessageBroker/Service2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+//Consumer
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Xml.Linq;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://xomvilok:bzp8Z98qipQkPmCF5a26xxwwoE1ejUOh@chimpanzee.rmq.cloudamqp.com/xomvilok");
+
+using IConnection connection = factory.CreateConnection();
+using IModel chanel = connection.CreateModel();
+
+chanel.ExchangeDeclare(exchange: "direct-exchande-example", type: ExchangeType.Direct);
+
+string queueName = chanel.QueueDeclare().QueueName;
+
+chanel.QueueBind(
+    queueName,
+    exchange: "direct-exchande-example",
+    routingKey: "direct-queue-example");
+
+EventingBasicConsumer conusumer = new(chanel);
+
+chanel.BasicConsume(
+    queue:queueName,
+     autoAck:true,
+     consumer:conusumer
+    );
+
+conusumer.Received += (send, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.ReadLine();
